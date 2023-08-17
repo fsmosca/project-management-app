9f836bac58b370481e51baa388aa82e691878ed2
fsmosca <fsmosca@users.noreply.github.com>
2023-08-17 10:17:53 +0800

    Create bootstrap.py

---
+++ modules/bootstrap.py
@@ -0,0 +1,19 @@
+"""Contains bootstrap 5 css and js cdn."""
+
+
+BOOTSTRAP_CSS = {
+        'rel': 'stylesheet',
+        'href': 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/'
+                'dist/css/bootstrap.min.css',
+        'integrity': 'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80'
+                     'zW1RWuH61DGLwZJEdK2Kadq2F9CUG65',
+        'crossorigin': 'anonymous'
+    }
+
+BOOTSTRAP_SCRIPT = {
+        'src': 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/'
+               'dist/js/bootstrap.bundle.min.js',
+        'integrity': 'sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9'
+                     'F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4',
+        'crossorigin': 'anonymous'
+    }
