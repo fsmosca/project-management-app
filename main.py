385e94e0544137abaf6987efa7592f11bf476889
fsmosca <fsmosca@users.noreply.github.com>
2023-08-17 10:17:59 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+"""This module contains the main entry point of the application."""
+
+
+from reactpy import component, html
+from reactpy_router import route, simple
+from reactpy.backend.fastapi import configure, Options
+from fastapi import FastAPI
+from modules.sqlmodeldb import create_db_and_tables
+from modules.home import Home
+from modules.blog import Blog
+from modules.missing import MissingLink
+from modules.task import (SearchTask, InputTask, DeleteTask,
+                          AddFabricationStatus, AddConstructionStatus)
+from modules.bootstrap import BOOTSTRAP_CSS, BOOTSTRAP_SCRIPT
+
+
+PAGE_HEADER_TITLE = 'Project Management System'
+
+
+create_db_and_tables()
+
+
+@component
+def Root():
+    return simple.router(
+        route('/', Home()),
+        route('/blog', Blog()),
+        route('/search-task', SearchTask()),
+        route('/input-task', InputTask()),
+        route('/delete-task', DeleteTask()),
+        route('/fabrication-status', AddFabricationStatus()),
+        route('/construction-status', AddConstructionStatus()),
+        route('*', MissingLink())
+    )
+
+
+app = FastAPI()
+configure(
+    app,
+    Root,
+    options=Options(
+        head=html.head(
+            html.link(BOOTSTRAP_CSS),
+            html.script(BOOTSTRAP_SCRIPT),
+            html.title(PAGE_HEADER_TITLE)
+        )
+    )
+)
