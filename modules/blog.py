e1e477cb4f145ad914d8d167b30c39574e1a3c22
fsmosca <fsmosca@users.noreply.github.com>
2023-08-17 10:17:56 +0800

    Create blog.py

---
+++ modules/blog.py
@@ -0,0 +1,55 @@
+from reactpy import component, html
+from modules.navbar import NavBar
+
+
+LABEL = 'Blog'
+
+
+@component
+def Blog_1():
+    title = 'Application Guide'
+    return html.div(
+        {'style': {'font-family': 'monospace'}},
+        html.h1(title),
+
+        html.h2('A. Tasks'),
+        html.p(
+            '''The navigation bar located at the top of the page has Home,
+            Blog, Task, Fabrication and Construction menu.'''
+        ),
+        html.ul(
+            html.li('Task - is a dropdown that contents "search task", "input task" and "delete task".'),
+            html.li('Fabrication - is also a dropdown dealing with fabrication-related interfaces.')
+        ),
+
+        html.h3('1. Search Task'),
+        html.p('''This is used to search tasks.'''
+        ),
+
+        html.h3('2. Input Task'),
+        html.p('''This is used to register the task in the sqlite database.
+                Task name must be unique.'''
+        ),
+    )
+
+
+@component
+def BlogContent():
+    return html.div(
+        html.div(
+            {'class': 'fw-bold fs-5 my-3'},
+            LABEL
+        ),
+        Blog_1()
+    )
+
+
+@component
+def Blog():
+    return html.div(
+        html.div(
+            {'class': 'container mt-3'},
+            NavBar({'Blog': True}),
+            BlogContent()
+        ),
+    )
