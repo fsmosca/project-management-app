ad5b0e1f8777d9eb1e62829cdcdd4f6367c1a664
fsmosca <fsmosca@users.noreply.github.com>
2023-08-20 16:57:58 +0800

    Update dashboard.py

--- modules/dashboard.py
+++ modules/dashboard.py
@@ -1,46 +1,165 @@
-from reactpy import component, html
+from io import StringIO
+
+from reactpy import component, html, utils
 from modules.navbar import NavBar
 from modules.task import sqlitedb_to_df, DataframeToVdom
 from modules.sqlmodeldb import select_all_tasks
+import plotly.express as px
+import pandas as pd
 
 
 LABEL = 'Dashboard'
 
 
+LINE_COLOR = {'Fabrication': 'blue', 'Construction': 'red'}
+
+
+def fabrication_progress(df, num_lines):
+    fab_cnt = len(df.loc[(df['category'] == 'Fabrication')
+                         & (df['status'] == 'DONE')])
+    return round(100 * fab_cnt / num_lines, 2)
+
+
+def construction_progress(df, num_lines):
+    con_cnt = len(df.loc[(df['category'] == 'Construction') &
+                         (df['status'] == 'DONE')])
+    return round(100 * con_cnt / num_lines, 2)
+
+
 @component
-def Linelist():
-    df = sqlitedb_to_df(select_all_tasks())
-    gb = df.groupby('task_name')
-    gbr = gb.last().reset_index()
+def LineChart(df, category, width=500, height=250):
+    df1 = df.copy()
+    df1['recorded_date'] = pd.to_datetime(df1['recorded_date'])
+    df1['week_number'] = df1['recorded_date'].dt.isocalendar().week
+    df1 = df1.astype({'progress': float})
+    df2 = df1.loc[(df1['category'] == category) & (df1['status'] == 'DONE')]
+    df3 = df2.groupby(by=['week_number'])['task_name'].count().reset_index()
+    df3['weekly_progress'] = df3['task_name'] / 12 * 100
+    df3['progress_%'] = df3['weekly_progress'].cumsum()
 
-    gbr = gbr.astype({'progress': float})
+    # Create a figure using the grouped dataframe.
+    fig = px.line(
+        df3, x='week_number', y='progress_%', markers=True,
+        width=width, height=height, title=category
+    )
 
-    # Fabrication
-    dffc = gbr.loc[gbr['category'] == 'Construction']
-    dff = gbr.loc[gbr['category'] == 'Fabrication']
-    total_progress = dff['progress'].sum() + 100 * len(dffc)
-    fabrication_pct_complete = round(100*total_progress / (100 * len(gbr)), 2)
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=100, b=20),
+        paper_bgcolor="#CEF5AF",
+    )
 
-    # Construction
-    dfc = gbr.loc[(gbr['category'] == 'Construction') & (gbr['progress'] >= 100)]
-    construction_pct_complete = round(100 * len(dfc) / len(gbr), 2)
+    fig.update_traces(line_color=LINE_COLOR[category], line_width=1)
+
+    # Create an html object in memory from fig.
+    buffer = StringIO()
+    fig.write_html(buffer, include_plotlyjs='cdn')
+    fig_html = buffer.getvalue()
+
+    return html.div(utils.html_to_vdom(fig_html))
+
+
+@component
+def BarChart(df, width=500, height=250):
+    """Creates a plotly bar chart."""
+    fig = px.bar(
+        df, x='progress_%', y='category', orientation='h', color='category',
+        width=width, height=height, text_auto=True,
+        title='Fabrication and Construction'
+    )
+
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=100, b=20),
+        paper_bgcolor="#CEF5AF",
+    )
+
+    fig.update_yaxes(visible=False, showticklabels=False)
+
+    # Create an html object in memory from fig.
+    buffer = StringIO()
+    fig.write_html(buffer, include_plotlyjs='cdn')
+    fig_html = buffer.getvalue()
 
     return html.div(
-        DataframeToVdom(gbr, height=200),
-        html.p({'class': 'mt-3'}, f'Fabrication Completion: {fabrication_pct_complete} %'),
-        html.p({'class': 'mt-3'}, f'Construction Completion: {construction_pct_complete} %')
+        utils.html_to_vdom(fig_html)
+    )
+
+
+def LineStatus(df):
+    return DataframeToVdom(df)
+
+
+def LineHoldStatus(df):
+    return DataframeToVdom(df)
+
+
+@component
+def Summary():
+    df = sqlitedb_to_df(select_all_tasks())
+    if len(df) < 1:
+        return
+    gb = df.groupby('task_name')
+    current_line_status = gb.last().reset_index()
+    num_lines = len(current_line_status)
+
+    # Plot fabrication and construction progress.
+    fab_done_pct = fabrication_progress(df, num_lines)
+    con_done_pct = construction_progress(df, num_lines)
+    progress_dict = {
+        'category': ['Fabrication', 'Construction'],
+        'progress_%': [fab_done_pct, con_done_pct]
+    }
+    df_fab_con_progress = pd.DataFrame(progress_dict)
+
+    # Task with hold current status.
+    df_hold = current_line_status.loc[current_line_status['status'] == 'HOLD']
+
+    return html.div(
+        html.div(
+            {'class': 'container'},
+            html.div(
+                {'class': 'row'},
+                html.div(
+                    {'class': 'col'},
+                    html.h5(
+                        {'class': 'mt-1'},
+                        'Overall Progress'
+                    ),
+                    BarChart(df_fab_con_progress, 400)
+                ),
+                html.div(
+                    {'class': 'col'},
+                    html.h5(
+                        {'class': 'mt-1'},
+                        'Fabrication Weekly Cummulative Progress'
+                    ),
+                    LineChart(df, 'Fabrication', 400)
+                ),
+                html.div(
+                    {'class': 'col'},
+                    html.h5(
+                        {'class': 'mt-1'},
+                        'Construction Weekly Cummulative Progress'
+                    ),
+                    LineChart(df, 'Construction', 400)
+                )
+            ),
+            html.div(
+                {'class': 'row'},
+                html.h5({'class': 'mt-3'}, 'Line current status'),
+                LineStatus(current_line_status)
+            ),
+            html.div(
+                html.h5({'class': 'mt-3'}, 'Lines with HOLD status'),
+                LineHoldStatus(df_hold)
+            )
+        )
     )
 
 
 @component
 def DashboardContent():
     return html.div(
-        html.div(
-            {'class': 'fw-bold fs-5 my-3'},
-            LABEL
-        ),
-        html.p('This is the latest status of each line.'),
-        Linelist(),
+        Summary()
     )
 
 
@@ -48,8 +167,14 @@ def DashboardContent():
 def Dashboard():
     return html.div(
         html.div(
-            {'class': 'container mt-4 pt-4'},
+            {'class': 'container'},
             NavBar({'Dashboard': True}),
-            DashboardContent()
-        ),
+            html.div(
+                html.div(
+                   {'class': 'fw-bold fs-5 mt-5 py-3 text-center'},
+                   LABEL
+                ),
+                DashboardContent()
+            )
+        )
     )
